c3d0f8daebc41096d531799fa5b3cd37d248441f
Tran Minh Hieu Dinh <jrvn-hieu@Hieus-MacBook-Pro.local>
2025-04-15 17:34:01 +0700

    add search with keyword

--- recommendation_engine/step_engine_music_with_rag_and_faiss.py
+++ recommendation_engine/step_engine_music_with_rag_and_faiss.py
@@ -6,6 +6,24 @@ import gradio as gr
 from sentence_transformers import SentenceTransformer
 import faiss
 from transformers import pipeline
+import argparse
+
+
+def args_parser():
+    option_parser = argparse.ArgumentParser(
+        description="Option to run"
+    )
+
+    option_parser.add_argument(
+        "--search-mode",
+        type=str,
+        help="value: album or keyword",
+        default="album"
+    )
+
+    args = option_parser.parse_args
+
+    return args
 
 
 def main():
@@ -42,8 +60,8 @@ def main():
     index.add(embeddings)
 
     # base_model = "google/flan-t5-large"
-    # alternative: deepseek-ai/DeepSeek-R1
-    generator = pipeline("text2text-generation", model='deepseek-ai/DeepSeek-R1')
+    # alternative: deepseek-ai/DeepSeek-R1 this need higher gpu
+    generator = pipeline("text2text-generation", model='google/flan-t5-large')
 
     def recommend_music(
         albums,
@@ -113,23 +131,74 @@ def main():
             )
         
         return app
-
-    # def search_music(query, model):
-    #     if not query:
-    #         return "Please enter a keyword to search"
+    
+    def search_music(query, model, faiss_index):
+        if not query:
+            return "Please enter a keyword to search"
         
-    #     query_embedding = model.encode([query], convert_to_numpy=True)
+        query_embedding = model.encode([query], convert_to_numpy=True)
+        _, recommended_indices = faiss_index.search(query_embedding, 10)
+        recommended_songs = data.iloc[
+            recommended_indices[0]
+        ][['Name', 'Artist', 'Album']].drop_duplicates()
+
+        song_list = "\n".join(
+            (
+                f"-{row['Name']} by {row['Artist']} (Album: {row['Album']})"
+                for _, row in recommended_songs.iterrows()
+            )
+        )
+
+        prompt = f"""
+        Recommend music based on the keyword: {query} \n{song_list}\n\n\
+        Give a short explanation
+        """
+        generated_text = generator(prompt, max_length=100)[0]['generated_text']
+
+        return f"""
+        ### Recommended songs:\n{song_list}\n\n
+        ### Why these songs?\n{generated_text}
+        """
+
+    def gradio_ui_for_music_with_keyword(faiss_index, generator):
+        def btn_search_music(query):
+            return search_music(query, model, faiss_index)
+
+        with gr.Blocks() as search_app:
+            gr.Markdown("Search Based Music Recommendation Engine")
+            query_input = gr.Textbox(label="Enter a keyword")
+            search_button = gr.Button("Search")
+            output_text = gr.Markdown()
+            search_button.click(
+                fn=btn_search_music,
+                inputs=[query_input],
+                outputs=[output_text]
+            ) 
+
+        return search_app
 
     unique_albums = data['Album'].dropna().unique().tolist()
 
-    gr_app = gradio_ui(
-        faiss_index=index,
-        generator=generator,
-        embeddings=embeddings,
-        unique_albums=unique_albums
-    )
+    system_option = args_parser()
+    gradio_mode = system_option.search_mode
 
-    gr_app.launch()
+    if gradio_mode == "album":
+        gr_app = gradio_ui(
+            faiss_index=index,
+            generator=generator,
+            embeddings=embeddings,
+            unique_albums=unique_albums
+        )
+
+        gr_app.launch()
+    
+    if gradio_mode == "keyword":
+        keyword_search_app = gradio_ui_for_music_with_keyword(
+            faiss_index=index,
+            generator=generator
+        )
+
+        keyword_search_app.launch()
 
 if __name__ == "__main__":
     main()
