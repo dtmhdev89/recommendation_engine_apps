80d4844021013cca08582df5ca2b27cd1b0a3291
Tran Minh Hieu Dinh <jrvn-hieu@Hieus-MacBook-Pro.local>
2025-04-21 15:11:34 +0700

    add semantic search and test

--- semantic_text_search_engine/ca_search_purchase_order/domain/purchase/PurchaseOrderWordEmbeddingsDomain.py
+++ semantic_text_search_engine/ca_search_purchase_order/domain/purchase/PurchaseOrderWordEmbeddingsDomain.py
@@ -17,7 +17,10 @@ class PurchaseOrderWordEmbeddingsDomain:
                 df["item_name_transformed"], convert_to_tensor=True
             )
 
-            save_path = os.path.join("data", "purchase-order-data-2012-2015-.pkl")
+            save_path = os.path.join(
+                "data",
+                "purchase-order-data-2012-2015-.pkl"
+            )
             with open(save_path, "wb") as fOut:
                 pickle.dump(
                     {'embeddings': corpus_embeddings},
@@ -29,3 +32,33 @@ class PurchaseOrderWordEmbeddingsDomain:
         except Exception as e:
             print("Error to generate word embeddings file: " + str(e))
             raise
+    
+    @staticmethod
+    def load_word_embeddings_file_transformed():
+        """Load serialized embeddings file"""
+        try:
+            save_path = os.path.join("data", "purchase-order-data-2012-2015-.pkl")
+            with open(save_path, "rb") as fIn:
+                stored_data = pickle.load(fIn)
+                corpus_embeddings = stored_data["embeddings"]
+
+                return corpus_embeddings
+        except Exception as e:
+            print("Error loading serialized Embeddings file: " + str(e))
+            raise
+    
+    @staticmethod
+    def transform_text_query_word_embeddings(query_embeddings):
+        """Transforms search query text into Word Embeddings"""
+
+        try:
+            model = SentenceTransformer('all-mpnet-base-v2')
+            query_embeddings = model.encode(
+                query_embeddings,
+                convert_to_tensor=True
+            )
+
+            return query_embeddings
+
+        except Exception as e:
+            print("Error transforming query text into Word Embeddings: " + str(e))
